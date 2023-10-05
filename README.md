8310cb24a37a522c25658f4a3247ffad528e82d4
aalemanyz67 <albertaleman67@gmail.com>
2023-10-04 22:02:52 -0500

    updated bootcamp quiz

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # BootCampQuiz-1
+Name: Alberto Aleman 
+Github: https://github.com/aalemanyz67/BootCampQuiz-1
+Website:
+
+# Objective:
 Quiz yourself and see if you know these bootcamp terms. Take the following 5 question quiz to test your knowledge.
+
+# How to use this application:
+Once webpage has been opened in the browswer click on start quiz button to begin quiz.
+Timer will begin to countdown and with every worng answer, 5 seconds will be deducted.
+
+# Technology and Resources:
+For this application I used the following resources.
+w3schools.com
+developer.mozilla.org
+youtube content creators:
+Brian Design
+GreatStack
+Web Cifar
+
+# Reflection:
+This application was especially challenging for me. Although I wasnt able to complete the application 100% I feel good at knowing that I am understanding the concepts learned in class more and more. With the help of my tutor I was able to inccorporate many of the functions and abilites used in javascript. I will continue to learn from this activity and further my skills and understanding of javascripts full application.
--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,7 @@ var endQuiz = document.querySelector("end-quiz");
 
 //start quiz function
 function startQuiz(){
+setTime()
 var titleEl = document.querySelector(".title");
 titleEl.classList.add("hidden")    
 var questionsEl = document.querySelector(".questions");
@@ -14,20 +15,65 @@ document.getElementById("continue-quiz").onclick=startQuiz;
     
  }
  document.getElementById("end-quiz").onclick=endQuiz;
-var countDown = document.querySelector(".seconds");
-var seconds = 60;
-var index = 1
-function changeQue() {
+
+//insert timer countdown
+ var countDown = document.querySelector(".seconds");
+ var secondsLeft = 60;
+
+ function setTime() {
+    
+    var timerInterval = setInterval(function(){
+        secondsLeft--;
+        countDown.textContent = secondsLeft;
+        //console.log(secondsLeft)
+
+        if(secondsLeft === 0){
+            clearInterval(timerInterval);
+            
+        }
+    }, 1000);
+ }
+ 
+ 
+var index = 0
+
+function changeQue() { 
+    console.log(myQuestions[index])
     var answer1 = document.querySelector("#answer-1");
     var answer2 = document.querySelector("#answer-2");
     var answer3 = document.querySelector("#answer-3");
     var answer4 = document.querySelector("#answer-4");
+    var answer5 = document.querySelector("#answer-5");
     answer1.textContent = myQuestions[index].answers["1"];
     answer2.textContent = myQuestions[index].answers["2"];
     answer3.textContent = myQuestions[index].answers["3"];
     answer4.textContent = myQuestions[index].answers["4"];
-
+    answer5.textContent = myQuestions[index].answers["5"];
+   
+var titleEl = document.querySelector(".question");
+titleEl.textContent = myQuestions[index].question
 } 
+
+//how to get correct or wrong answers and score
+document.querySelectorAll(".answers").forEach(function(answer){
+answer.onclick = function(event){
+    console.log(event.target)
+    var chosen = event.target.textContent
+    var correct = myQuestions[index].correctAnswer
+    console.log("correctAnswer "+ correct)
+    console.log("chosen " + chosen )
+   if(correct == chosen){
+    index++
+    changeQue()
+
+   }else{
+    secondsLeft = secondsLeft -5
+    index++
+    changeQue()
+   }
+    
+}
+})
 
 //Quiz Questions
 var myQuestions = [
@@ -40,7 +86,9 @@ var myQuestions = [
            4: "Cascading Style Software"
             
         },
-        correctAnswer: "2"
+        correctAnswer: "Cascading Style Sheets",
+        correct: true,
+        incorrect: false,
     },
     {
         question: "What does HTML stand for?",
@@ -50,7 +98,8 @@ var myQuestions = [
             3: "Hypertext Markdown Language",
             4: "Hypertext Markdown Levels"
 
-        }
+        },
+        correctAnswer: "Hypertext Markup Language",
     },
     {
         question: "What do you call people who write computer code?",
@@ -59,7 +108,8 @@ var myQuestions = [
             2: "Cryptographers",
             3: "Programers or Developers",
             4: "Technology Professor"
-        }
+        },
+        correctAnswer: "Programers or Developers",
     },
     {
         question: "What does the word dev mean?",
@@ -68,15 +118,21 @@ var myQuestions = [
             2: "devocalize",
             3: "developer",
             4: "devalue"
-        }
+        },
+        correctAnswer: "developer",
     },
     {
         question: "What does coding do?",
-        answer: {
+        answers: {
             1: "list of functions",
             2: "never changes",
             3: "never updates",
             4: "tells the computer what to do"
-        }
-    }
+        },
+        correctAnswer: "tells the computer what to do",
+    },
 ]
+//var initialsInput = document.querySelector("#initials");
+//var signupButton = document.querySelector
+//
+//when quiz is done show input for initials and score storage
